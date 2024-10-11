de4a9681da379226a61d9077c51f748cc98c9aea
Hendrik Jaks <hendrikjaks@gmail.com>
2024-10-11 23:35:45 +0300

    testin

--- test.py
+++ test.py
@@ -3,15 +3,5 @@ a = "hennu"
 
 
 HENNU
-
-a = 'henna!'
-if a == 'henna!':
-    for i in range(1011):
-        print(i)
-    else:
-        pass
-"Miks see ei tööta"
-proov
-commit1 = commit
-
-,mida sa kammid
+Mis s ateeed
+MAIN
