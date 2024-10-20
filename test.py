e48de3fb26857e4ab04c11c3aa415ec0210f8421
f4lt3ring <57318281+f4lt3ring@users.noreply.github.com>
2024-10-20 14:01:42 +0300

    suht segane

--- test.py
+++ test.py
@@ -1,3 +1,24 @@
 print("Hello World")
 for i in range(10):
     print(str(i) + " Hello world")
+
+#virtualenv 20.27.0
+
+#Flask version 3.0.3
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def hello_world():
+    return "Hello World"
+
+if __name__ == '__main__':
+    app.run()
+"""
+#debug function
+app.debug=True
+app.run()
+app.run(debug=True)
+
+#externally visible server
+app.run(host='0.0.0.0')
+"""
